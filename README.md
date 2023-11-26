c989ffec80f252013a7db6a409ebd53cfdaa17ac
YelovSK <hampatrik@gmail.com>
2023-11-26 15:02:47 +0100

    typescript

---
+++ README.md
@@ -0,0 +1,5 @@
+I'm too lazy to right-click an image and click 'Open in new tab', so I wrapped all img tags with anchor tags.
+
+# Build
+
+`npm run build` (Windows only)
--- src/content.ts
+++ src/content.ts
@@ -6,46 +6,46 @@ function wrapImages() {
     }
 }
 
-function wrapImage(image) {
-    if (image.parentNode === undefined) {
+function wrapImage(image: HTMLImageElement) {
+    if (image.parentElement == null) {
         return;
     }
 
     // Image only website
-    if (image.parentNode.tagName.toLowerCase() === 'body' && image.parentNode.childNodes.length === 1) {
+    if (image.parentElement instanceof HTMLBodyElement && image.parentElement.childNodes.length === 1) {
         return;
     }
 
     // Image is already wrapped
-    if (image.parentNode.tagName.toLowerCase() === 'a') {
+    if (image.parentElement instanceof HTMLAnchorElement) {
         return;
     }
 
     // Check if the image is already wrapped multiple layers above
     // Assume the parent is the same size as the image
-    let currentParent = image.parentNode;
+    let currentParent: HTMLElement | null = image.parentElement;
     while (true) {
         // Reached the top
-        if (currentParent === undefined) {
+        if (currentParent == null) {
             break;
         }
 
         // Different size than the image
-        if (currentParent?.clientWidth !== image.clientWidth || currentParent?.clientHeight !== image.clientHeight) {
+        if (currentParent.clientWidth !== image.clientWidth || currentParent.clientHeight !== image.clientHeight) {
             break;
         }
 
         // Found a link that wraps the image
-        if (currentParent.tagName.toLowerCase() === 'a') {
+        if (currentParent instanceof HTMLAnchorElement) {
             return;
         }
 
-        currentParent = currentParent.parentNode;
+        currentParent = currentParent.parentElement;
     }
 
     // Save original styles
     const computedStyle = window.getComputedStyle(image);
-    const style = {};
+    const style: { [key: string]: string } = {};
     for (const key of Array.from(computedStyle)) {
         style[key] = computedStyle.getPropertyValue(key);
     }
@@ -53,22 +53,22 @@ function wrapImage(image) {
     const wrapper = document.createElement('a');
     wrapper.href = image.src;
 
-    image.parentNode.insertBefore(wrapper, image);
+    image.parentElement.insertBefore(wrapper, image);
     wrapper.appendChild(image);
 
     // Apply styles
-    for (const [key, value] of Object.entries(style)) {
-        image.style.setProperty(key, value);
+    for (const key of Object.keys(style)) {
+        image.style.setProperty(key, style[key]);
     }
     image.style.setProperty('cursor', 'pointer');
 }
 
-function handleMutations(mutations) {
+function handleMutations(mutations: MutationRecord[]) {
     for (const mutation of mutations) {
         if (mutation.type !== 'childList') continue;
 
         for (const node of mutation.addedNodes) {
-            if (node.tagName?.toLowerCase() === 'img') {
+            if (node instanceof HTMLImageElement) {
                 node.addEventListener('load', () => {
                     wrapImage(node);
                 });
@@ -80,5 +80,5 @@ function handleMutations(mutations) {
 wrapImages();
 
 // Subscribe to newly added images
-const observer = new MutationObserver(handleMutations);
+const observer: MutationObserver = new MutationObserver(handleMutations);
 observer.observe(document.body, { childList: true, subtree: true });
